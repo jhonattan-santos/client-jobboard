dba865c5031eb5750e3fa1158162c759d1078042
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2022-12-11 00:12:23 -0300

    changed the form component to use a custom hook called createJob

--- src/components/JobForm.js
+++ src/components/JobForm.js
@@ -1,50 +1,54 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import { createJob } from '../graphql/queries';
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useCreateJob } from "../graphql/hooks";
 
 function JobForm() {
   const navigate = useNavigate();
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const { createJob, loading } = useCreateJob();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const job = await createJob({ title, description });
+    const job = await createJob(title, description);
     if (job) {
-      navigate(`/jobs/${job.id}`)
+      navigate(`/jobs/${job.id}`);
     }
   };
 
   return (
     <div>
-      <h1 className="title">
-        New Job
-      </h1>
-      <div className="box">
+      <h1 className='title'>New Job</h1>
+      <div className='box'>
         <form>
-          <div className="field">
-            <label className="label">
-              Title
-            </label>
-            <div className="control">
-              <input className="input" type="text" value={title}
+          <div className='field'>
+            <label className='label'>Title</label>
+            <div className='control'>
+              <input
+                className='input'
+                type='text'
+                value={title}
                 onChange={(event) => setTitle(event.target.value)}
               />
             </div>
           </div>
-          <div className="field">
-            <label className="label">
-              Description
-            </label>
-            <div className="control">
-              <textarea className="textarea" rows={10} value={description}
+          <div className='field'>
+            <label className='label'>Description</label>
+            <div className='control'>
+              <textarea
+                className='textarea'
+                rows={10}
+                value={description}
                 onChange={(event) => setDescription(event.target.value)}
               />
             </div>
           </div>
-          <div className="field">
-            <div className="control">
-              <button className="button is-link" onClick={handleSubmit}>
+          <div className='field'>
+            <div className='control'>
+              <button 
+                className='button is-link'
+                disabled={loading} 
+                onClick={handleSubmit}>
                 Submit
               </button>
             </div>
--- src/graphql/hooks.js
+++ src/graphql/hooks.js
@@ -1,20 +1,15 @@
-import { useQuery } from "@apollo/client";
-import { COMPANY_QUERY, JOBS_QUERY, JOB_QUERY } from "./queries";
-
-export function useCompany(companyId) {
-  const { data, error, loading } = useQuery(COMPANY_QUERY, {
-    variables: { companyId }
-  });
-  return {
-    company: data?.company,
-    error,
-    loading,
-  };
-}
+import { useMutation, useQuery } from "@apollo/client";
+import { getAccessToken } from "../auth";
+import {
+  COMPANY_QUERY,
+  CREATE_JOB_MUTATION,
+  JOBS_QUERY,
+  JOB_QUERY,
+} from "./queries";
 
 export function useJob(id) {
   const { data, error, loading } = useQuery(JOB_QUERY, {
-    variables: { id }
+    variables: { id },
   });
   return {
     job: data?.job,
@@ -29,6 +24,40 @@ export function useJobs() {
   });
   return {
     jobs: data?.jobs,
+    error,
+    loading,
+  };
+}
+
+export function useCreateJob() {
+  const [mutate, { loading }] = useMutation(CREATE_JOB_MUTATION);
+  return {
+    createJob: async (title, description) => {
+      const { data: { job } } = await mutate({
+        variables: { input: { title, description } },
+        context: {
+          headers: { Authorization: `Bearer ${getAccessToken()}` },
+        },
+        update: (cache, { data: { job } }) => {
+          cache.writeQuery({
+            query: JOB_QUERY,
+            variables: { id: job.id },
+            data: { job },
+          });
+        },
+      });
+      return job;
+    },
+    loading
+  };
+}
+
+export function useCompany(companyId) {
+  const { data, error, loading } = useQuery(COMPANY_QUERY, {
+    variables: { companyId },
+  });
+  return {
+    company: data?.company,
     error,
     loading,
   };
